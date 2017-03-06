7f2e2a8736f80f57762f4dd9578bde40b4fb292b
Vasily Bezruchkin <vbezruchkin@intelliants.com>
2017-03-06 14:48:59 +0600

    Plugin ready.

--- includes/hook.phpPackageItemChangedForPlugin.php
+++ includes/hook.phpPackageItemChangedForPlugin.php
@@ -1,5 +1,28 @@
 <?php
-//##copyright##
+/******************************************************************************
+ *
+ * Subrion - open source content management system
+ * Copyright (C) 2017 Intelliants, LLC <https://intelliants.com>
+ *
+ * This file is part of Subrion.
+ *
+ * Subrion is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Subrion is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Subrion. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * @link https://subrion.org/
+ *
+ ******************************************************************************/
 
 if (iaView::REQUEST_HTML == $iaView->getRequestType())
 {
@@ -11,8 +34,10 @@ if (iaView::REQUEST_HTML == $iaView->getRequestType())
 	$plugin = 'youtube';
 	$field = 'youtube_video';
 	$pages = array(
-		'accounts' => array('view_member', 'profile'),
+		'members' => array('view_member', 'profile'),
 		'autos' => array('autos_add', 'autos_edit'),
+		'autos_parts' => array('autos_part_add', 'autos_part_edit'),
+		'autos_services' => array('autos_service_add', 'autos_service_edit'),
 		'articles' => array('submit_article', 'edit_article'),
 		'estates' => array('estate_submit', 'estate_edit'),
 		'listings' => array('add_listing', 'edit_listing')
@@ -21,6 +46,7 @@ if (iaView::REQUEST_HTML == $iaView->getRequestType())
 	foreach ($data as $node)
 	{
 		$item = $node['item'];
+		$langKey = 'field_' . $item . '_youtube_video';
 		switch($node['action'])
 		{
 			case '+':
@@ -53,6 +79,8 @@ if (iaView::REQUEST_HTML == $iaView->getRequestType())
 						}
 						$iaDb->insert($rows, false, 'fields_pages');
 					}
+
+					iaLanguage::addPhrase($langKey, iaLanguage::get('youtube_video'), '', 'youtube');
 				}
 				break;
 
--- templates/front/tab.tpl
+++ templates/front/tab.tpl
@@ -1,10 +1,10 @@
-{if isset($item.youtube_video) && $item.youtube_video && isset($youtube_video)}
+{if !empty($item.youtube_video) && isset($youtube_video)}
 	{capture append='tabs_content' name='youtube_video'}
 		<div id="youtube_video" class="ia-wrap text-center">
 			<object width="425" height="355">
-				<param name="movie" value="http://www.youtube.com/v/{$youtube_video}&rel=1"></param>
+				<param name="movie" value="https://www.youtube.com/v/{$youtube_video}&rel=1"></param>
 				<param name="wmode" value="transparent"></param>
-				<embed src="http://www.youtube.com/v/{$youtube_video}&rel=1" type="application/x-shockwave-flash" wmode="transparent" allowfullscreen="true" width="425" height="355"></embed>
+				<embed src="https://www.youtube.com/v/{$youtube_video}&rel=1" type="application/x-shockwave-flash" wmode="transparent" allowfullscreen="true" width="425" height="355"></embed>
 			</object>
 		</div>
 	{/capture}
