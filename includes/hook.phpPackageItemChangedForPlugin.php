23c9a271c6b22ad7e818cfb56d4841fa50083d74
Vasily Bezruchkin <vbezruchkin@intelliants.com>
2017-05-31 14:34:25 +0600

    Valid RSS feed.

--- includes/hook.phpPackageItemChangedForPlugin.php
+++ includes/hook.phpPackageItemChangedForPlugin.php
@@ -24,68 +24,60 @@
  *
  ******************************************************************************/
 
-if (iaView::REQUEST_HTML == $iaView->getRequestType())
-{
-	if (empty($data))
-	{
-		return;
-	}
+if (iaView::REQUEST_HTML == $iaView->getRequestType()) {
+    if (empty($data)) {
+        return;
+    }
 
-	$plugin = 'youtube';
-	$field = 'youtube_video';
-	$pages = [
-		'members' => ['view_member', 'profile'],
-		'autos' => ['autos_add', 'autos_edit'],
-		'autos_parts' => ['autos_part_add', 'autos_part_edit'],
-		'autos_services' => ['autos_service_add', 'autos_service_edit'],
-		'articles' => ['submit_article', 'edit_article'],
-		'estates' => ['estate_submit', 'estate_edit'],
-		'listings' => ['add_listing', 'edit_listing']
-	];
+    $plugin = 'youtube';
+    $field = 'youtube_video';
+    $pages = [
+        'members' => ['view_member', 'profile'],
+        'autos' => ['autos_add', 'autos_edit'],
+        'autos_parts' => ['autos_part_add', 'autos_part_edit'],
+        'autos_services' => ['autos_service_add', 'autos_service_edit'],
+        'articles' => ['submit_article', 'edit_article'],
+        'estates' => ['estate_submit', 'estate_edit'],
+        'listings' => ['add_listing', 'edit_listing'],
+    ];
 
-	foreach ($data as $node)
-	{
-		$item = $node['item'];
-		$langKey = 'field_' . $item . '_youtube_video';
-		switch($node['action'])
-		{
-			case '+':
-				if($iaDb->exists('`item` = :item AND `name` = :field_name', ['item' => $item, 'field_name' => $field], 'fields'))
-				{
-					$iaDb->update(['status' => iaCore::STATUS_ACTIVE], "`name` = '$field' AND `item` = '$item'", null, 'fields');
-				}
-				else
-				{
-					$sql = sprintf("ALTER TABLE `%s%s` ADD `%s` VARCHAR(%d) NOT NULL DEFAULT ''", $iaCore->iaDb->prefix, $item, $field, 128);
-					$iaDb->query($sql);
+    foreach ($data as $node) {
+        $item = $node['item'];
+        $langKey = 'field_' . $item . '_youtube_video';
+        switch ($node['action']) {
+            case '+':
+                if ($iaDb->exists('`item` = :item AND `name` = :field_name', ['item' => $item, 'field_name' => $field], 'fields')) {
+                    $iaDb->update(['status' => iaCore::STATUS_ACTIVE], "`name` = '$field' AND `item` = '$item'", null, 'fields');
+                } else {
+                    $itemTable = $iaCore->factory('item')->getItemTable($item);
+                    $sql = sprintf("ALTER TABLE `%s%s` ADD `%s` VARCHAR(%d) NOT NULL DEFAULT ''", $iaCore->iaDb->prefix, $itemTable, $field, 128);
+                    $iaDb->query($sql);
 
-					$id = $iaDb->insert([
-						'module' => $plugin,
-						'item' => $item,
-						'name' => $field,
-						'type' => 'text',
-						'length' => 128,
-						'status' => iaCore::STATUS_ACTIVE
-					], false, 'fields');
-					if (isset($pages[$item]))
-					{
-						$rows = [];
-						foreach ($pages[$item] as $page)
-						{
-							$rows[] = [
-								'page_name' => $page,
-								'field_id' => $id
-							];
-						}
-						$iaDb->insert($rows, false, 'fields_pages');
-					}
+                    $id = $iaDb->insert([
+                        'module' => $plugin,
+                        'item' => $item,
+                        'name' => $field,
+                        'type' => 'text',
+                        'length' => 128,
+                        'status' => iaCore::STATUS_ACTIVE
+                    ], false, 'fields');
+                    if (isset($pages[$item])) {
+                        $rows = [];
+                        foreach ($pages[$item] as $page) {
+                            $rows[] = [
+                                'page_name' => $page,
+                                'field_id' => $id
+                            ];
+                        }
+                        $iaDb->insert($rows, false, 'fields_pages');
+                    }
 
-					iaLanguage::addPhrase($langKey, iaLanguage::get('youtube_video'), '', 'youtube');
-				}
-				break;
+                    iaLanguage::addPhrase($langKey, iaLanguage::get('youtube_video'), '', 'youtube');
+                }
+                break;
 
-			case '-':
-				$iaDb->update(['status' => iaCore::STATUS_APPROVAL], "`name` = '$field' && `item` = '$item'", null, 'fields');
-		}
-	}
+            case '-':
+                $iaDb->update(['status' => iaCore::STATUS_APPROVAL], "`name` = '{$field}' && `item` = '{$item}'", null, 'fields');
+        }
+    }
 }
--- templates/front/tab.tpl
+++ templates/front/tab.tpl
@@ -1,11 +1,11 @@
 {if !empty($item.youtube_video) && isset($youtube_video)}
-	{capture append='tabs_content' name='youtube_video'}
-		<div id="youtube_video" class="ia-wrap text-center">
-			<object width="425" height="355">
-				<param name="movie" value="https://www.youtube.com/v/{$youtube_video}&rel=1"></param>
-				<param name="wmode" value="transparent"></param>
-				<embed src="https://www.youtube.com/v/{$youtube_video}&rel=1" type="application/x-shockwave-flash" wmode="transparent" allowfullscreen="true" width="425" height="355"></embed>
-			</object>
-		</div>
-	{/capture}
+    {capture append='tabs_content' name='youtube_video'}
+        <div id="youtube_video" class="ia-wrap text-center">
+            <object width="425" height="355">
+                <param name="movie" value="https://www.youtube.com/v/{$youtube_video}&rel=1"></param>
+                <param name="wmode" value="transparent"></param>
+                <embed src="https://www.youtube.com/v/{$youtube_video}&rel=1" type="application/x-shockwave-flash" wmode="transparent" allowfullscreen="true" width="425" height="355"></embed>
+            </object>
+        </div>
+    {/capture}
 {/if}
